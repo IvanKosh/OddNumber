3547884071e117bc75eb1ceecac3eef54cc76652
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-17 17:45:15 +0300

    finish

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: bat
+ *
+ * Created on 17 Сентябрь 2015 г., 17:43
+ */
+
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 99; i+=2)
+        printf("%i\n", i);
+    return 0;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=oddnumber
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/oddnumber
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=oddnumber.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/oddnumber.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=oddnumber
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/oddnumber
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=oddnumber.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/oddnumber.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
